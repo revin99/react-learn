b33f341057399c119f8d82e2207893960aae7897
revin99 <revingohil@gmail.com>
2025-07-05 11:25:35 +0530

    Functional component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import Welcome from './components/Welcome';
 
 function App() {
   return (
     <div className="App">
-      <h1> Hello world</h1>
+      <Welcome name="Ramesh"/>
+      <Welcome name ="Revin"/>
     </div>
   );
 }
---
+++ src/components/Welcome.js
@@ -0,0 +1,9 @@
+// function Welcome(){
+//     return <h1>Welcome Revin</h1>
+// }
+
+// export default Welcome
+
+const Welcome = (props) =>  <h1>Welcome {props.name}</h1>
+
+export default Welcome
